23b98cae0780ff3c5acb8cd6401fea335b067936
Trevor Cooper <tcooper@sdsc.edu>
2013-11-04 15:36:52 -0800

    Controlling build order of packages

--- src/linux.mk
+++ src/linux.mk
@@ -1,4 +1,7 @@
 SRCDIRS = `find * -prune\
 	  -type d 	\
+          ! -name python-2  \
+          ! -name python-3  \
+          ! -name python-modules  \
 	  ! -name CVS	\
-	  ! -name .`
+          ! -name .` python-3 python-2 python-modules
