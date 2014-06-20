e150fc4be24175feeeb14ba1f3bd00ef8d3a6b6b
Luca Clementi <luca.clementi@gmail.com>
2014-06-19 17:42:06 -0700

    bootstrap: warn user if rocks-devel is too old
    with this new binaryless repo you need rocks-devel-6.2 to be
    able to download the binaries
    
    I used the following script to modify the bootstrap.sh
    
    awk '/. \$ROLLSROOT\/etc\/bootstrap-functions.sh/{print "if [ ! -f \"$ROLLSROOT/../../bin/get_sources.sh\" ]; then\n\techo \"To compile this roll on Rocks 6.1.1 or older you need to install a newer rocks-devel rpm.\nInstall it with:\nrpm -Uvh https://googledrive.com/host/0B0LD0shfkvCRRGtadUFTQkhoZWs/rocks-devel-6.2-3.x86_64.rpm\nIf you need an older version of this roll you can get it from:\nhttps://github.com/rocksclusters-attic\"\n\texit 1\nfi\n"}1' bootstrap.sh > tmp
      cp tmp bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -72,7 +72,16 @@
 # and for the bio roll
 #
 
-. $ROCKSROOT/src/roll/etc/bootstrap-functions.sh
+if [ ! -f "$ROLLSROOT/../../bin/get_sources.sh" ]; then
+	echo "To compile this roll on Rocks 6.1.1 or older you need to install a newer rocks-devel rpm.
+Install it with:
+rpm -Uvh https://googledrive.com/host/0B0LD0shfkvCRRGtadUFTQkhoZWs/rocks-devel-6.2-3.x86_64.rpm
+If you need an older version of this roll you can get it from:
+https://github.com/rocksclusters-attic"
+	exit 1
+fi
+
+. $ROLLSROOT/etc/bootstrap-functions.sh
 
 
 #this is required only to compile sqlite3 support in python
