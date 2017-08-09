f3473ab21385489796d87013c0aedba84f039469
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2017-08-09 08:59:14 -0700

    Updated version of Python2.

--- src/python-2/version.mk
+++ src/python-2/version.mk
@@ -1,6 +1,18 @@
 NAME		= opt-python-27
 PKGROOT		= /opt/python
+PYPKG		= python2.7
 ARCHIVENAME    	= Python
-VERSION 	= 2.7.9
+VERSION 	= 2.7.13
 RELEASE 	= 1
 TARBALL_POSTFIX	= tgz
+RPM.EXTRAS = \
+%define _python_bytecompile_errors_terminate_build 0\\n \
+%define __python %{buildroot}/$(PKGROOT)/bin/python \\n \
+%define __python_requires  %{_builddir}/%{name}-%{version}/filter_python_requires.sh
+RPM.FILES	= \
+$(PKGROOT)/lib/* \\n \
+$(PKGROOT)/bin/* \\n \
+$(PKGROOT)/include/* \\n \
+$(PKGROOT)/share/man/man1/* \\n \
+/etc/profile.d/*
+
