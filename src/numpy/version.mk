4b3ae774edc2ca969a17697ca7349aff30b40ec0
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2017-08-10 06:29:31 -0700

    Add description and requires

--- src/numpy/version.mk
+++ src/numpy/version.mk
@@ -5,3 +5,5 @@ RELEASE		= 1
 RPM.FILES	= \
 /opt/python/lib/python2.7/site-packages/*\\n\
 /opt/python/bin/*
+RPM.REQUIRES	= opt-python-27
+RPM.DESCRIPTION = NumPy is the fundamental package needed for scientific computing with Python. This package contains: a powerful N-dimensional array object, sophisticated (broadcasting) functions, tools for integrating C/C++ and Fortran code, and useful linear algebra, Fourier transform, and random number capabilities.
