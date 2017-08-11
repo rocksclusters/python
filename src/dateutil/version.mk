dcbe042f829cd40aea2945a3152b1cd826b38c51
root <root@rocce-vm6.ucsd.edu>
2017-08-11 12:32:35 -0700

    add dateutil package

---
+++ src/dateutil/version.mk
@@ -0,0 +1,7 @@
+NAME        = opt-dateutil-27
+VERSION     = 2.6.1
+ARCHIVENAME = python-dateutil
+RELEASE	    = 0
+PYTHON      = python2.7
+RPM.FILES 	= \
+/opt/python/lib/$(PYTHON)/site-packages/*
