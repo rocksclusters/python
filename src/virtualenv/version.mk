df1a4572f5077bca12349a5cdddbda36a8e0a16a
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2017-08-15 10:47:18 -0700

    add virtualenv as a package. Basically, enable users to utilize pip and other tools into a user-customized Python environment

---
+++ src/virtualenv/version.mk
@@ -0,0 +1,8 @@
+NAME        = opt-virtualenv
+VERSION     = 15.1.0
+ARCHIVENAME = virtualenv
+RELEASE	    = 0
+PYTHON      = python2.7
+RPM.FILES   = \
+/opt/python/lib/$(PYTHON)/site-packages/*\\n\
+/opt/python/bin/*
