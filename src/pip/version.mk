2cffe94c6067ed83e9a7260c3479c7a9f207da40
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2017-08-15 11:08:46 -0700

    Add pip for opt-python-27 (already part of python3)

---
+++ src/pip/version.mk
@@ -0,0 +1,8 @@
+NAME        = opt-pip
+VERSION     = 9.0.1
+ARCHIVENAME = pip
+RELEASE	    = 0
+PYTHON      = python2.7
+RPM.FILES   = \
+/opt/python/lib/$(PYTHON)/site-packages/*\\n\
+/opt/python/bin/*
